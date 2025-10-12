ec19a7b74a54ef05cd0cb0c482aa90f8104273d6
nullture <ysaandre.mendoza@gmail.com>
2025-10-13 02:29:45 +0800

    progress on appointments and profiles

---
+++ src/components/dashboard/doctor/ManagementProfile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ManagementProfile() {
+  return (
+    <div>
+      <h1>Management Profile</h1>
+      <p>Admin details and management profile info go here.</p>
+    </div>
+  );
+}
